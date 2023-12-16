7c6649a8afb5954a065c64ddf8393ff51c21e283
mattlusty <mattlusty123@gmail.com>
2023-12-16 21:27:58 +0000

    Added js for processing weather days

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let searchInput = $("#search-input");
 let baseUrl = "https://api.openweathermap.org/data/2.5/forecast?";
-
 let appId = "&appid=9397f4c4feeadfc01afd4e19fa302fb4";
 
 let history = [];
+
+// Elements
+let forecastSection = $("#forecast");
 
 let searchButton = $("#search-button").on("click", function (event) {
   event.preventDefault();
@@ -20,16 +22,65 @@ let searchButton = $("#search-button").on("click", function (event) {
 
   fetch(queryUrl)
     .then(function (response) {
-      console.log(response);
       return response.json();
     })
     .then(function (data) {
       let items = data.list;
+      console.log(items);
+
+      let days = [[]];
+      console.log("days: ", days);
+      let loopDay = dayjs().format("dddd");
+      let loopDayIndex = 0;
+
+      console.log("First loopDay: ", loopDay);
+
       // 5 items = next 15 hours
-      for (let i = 0; i < 5; i++) {
+      for (let i = 0; i < items.length; i++) {
+        console.log("i: ", i);
+
         let item = items[i];
-        let day = dayjs(item.dt * 1000).format("YYYY-MM-DD HH:mm");
-        console.log(day);
+
+        let dateSeconds = item.dt;
+        let dateMilliseconds = dateSeconds * 1000;
+        let date = dayjs(dateMilliseconds);
+        let weekDay = date.format("dddd");
+        // let hour = date.format("HH:00"); // DELETE
+
+        let tempKelvin = item.main.temp;
+        let temp = (tempKelvin - 273.15).toFixed(2);
+
+        let day = { day: weekDay, temp: temp };
+
+        console.log("weekDay: ", weekDay);
+        console.log("loopDay: ", loopDay);
+        console.log("weekDay == loopDay: ", weekDay == loopDay);
+
+        if (!(weekDay == loopDay)) {
+          console.log("new day!");
+          loopDayIndex++;
+          loopDay = weekDay;
+          days[loopDayIndex] = [];
+        }
+
+        days[loopDayIndex].push(day);
+
+        renderDay(day);
       }
     });
 });
+
+function renderDay({ day, temp, wind, humidity }) {
+  // Construct elements
+  let forecastElement = $("<div>");
+  let weekDayElement = $("<div>").text(day);
+  let tempElement = $("<div>").text(temp);
+  let windElement = $("<div>").text(day);
+  let himidityElement = $("<div>").text(humidity);
+  // Apply classes
+  forecastElement.addClass("col m-2 border border-dark");
+  // Construct
+  forecastElement.append(weekDayElement, tempElement, windElement, himidityElement);
+  // Append to DOM
+  forecastSection.append(forecastElement);
+}
